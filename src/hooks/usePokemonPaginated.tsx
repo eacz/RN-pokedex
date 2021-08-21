378a99c82b20871bd6d11ffa3d8f4bc992cd1fcf
Esteban Canteros <estebanacanteros@gmail.com>
2021-08-21 18:43:55 -0300

    custom hook usePokemonPaginated

---
+++ src/hooks/usePokemonPaginated.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useRef, useEffect } from "react"
+import pokemonApi from "../api/pokemonApi"
+import { PokemonPaginatedResponse, SimplePokemon, Result } from '../interfaces/pokemonInterfaces';
+
+const usePokemonPaginated = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([])
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
+
+  const loadPokemons = async () => {
+    setIsLoading(true)
+    const res = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = res.data.next;
+    mapPokemonListToSimplePokemon(res.data.results)
+  }
+  
+  const mapPokemonListToSimplePokemon = (pokemonList: Result[]) => {
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({name, url}) => {
+      const urlParts = url.split('/')
+      const id = urlParts[urlParts.length-2];
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+      return { id, picture, name }
+    })
+    setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+    setIsLoading(false)
+  }
+
+
+  useEffect(() => {
+    loadPokemons();
+  }, [])
+
+  return {
+    isLoading,
+    simplePokemonList
+  }
+
+}
+
+export default usePokemonPaginated
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Text, Image } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import usePokemonPaginated from '../hooks/usePokemonPaginated'
 import globalStyles from '../theme/appTheme'
 
 const HomeScreen = () => {
   const { top } = useSafeAreaInsets()
+  const {simplePokemonList, isLoading} = usePokemonPaginated()
+  
   return (
     <>
       <Image style={globalStyles.pokeballBG} source={require('../assets/pokebola.png')} />
